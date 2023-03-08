3de80736fd14ca1c7c1aaa7ccc885b5087daf326
José Dias <37698268+josdias@users.noreply.github.com>
2023-03-08 15:54:34 -0300

    Add files via upload
    Alteração de layout, design mais responsivo, edição de bugs e botão voltar habilitado.

--- src/js/carrosel.js
+++ src/js/carrosel.js
@@ -2,3 +2,7 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 10,
     slidesPerView: 1.5,
   });
+
+  document.getElementById("voltar").addEventListener("click", function() {
+    window.history.back();
+  });
